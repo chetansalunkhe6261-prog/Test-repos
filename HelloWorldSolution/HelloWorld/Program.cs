8ef89658a54e5e85b1052a21c0dc8389f6f8b8f7
chetansalunkhe6261-prog <chetan.salunkhe6261@gmail.com>
2026-09-04 13:35:05 +0530

    update some more code

--- HelloWorldSolution/HelloWorld/Program.cs
+++ HelloWorldSolution/HelloWorld/Program.cs
@@ -1,3 +1,8 @@
 Console.WriteLine("Hello World!");
 Console.Write("Hello World!");
+<<<<<<< Updated upstream
 Console.Write("Welcome to the world");
+=======
+Console.WriteLine("Hello Developer");
+
+>>>>>>> Stashed changes
