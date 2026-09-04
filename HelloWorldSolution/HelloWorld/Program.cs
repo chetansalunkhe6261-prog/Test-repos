500d22460eccbafa8ebf892026d4559e30667783
chetansalunkhe6261-prog <chetan.salunkhe6261@gmail.com>
2026-09-04 13:41:40 +0530

    Update Program.cs

--- HelloWorldSolution/HelloWorld/Program.cs
+++ HelloWorldSolution/HelloWorld/Program.cs
@@ -1,8 +1,5 @@
 Console.WriteLine("Hello World!");
 Console.Write("Hello World!");
-<<<<<<< Updated upstream
 Console.Write("Welcome to the world");
-=======
 Console.WriteLine("Hello Developer");
 
->>>>>>> Stashed changes
