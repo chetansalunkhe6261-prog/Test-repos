ca1164e771ed08d2206d34f20b048fb9f5ded6cf
chetansalunkhe6261-prog <chetan.salunkhe6261@gmail.com>
2026-09-04 15:36:55 +0530

    changes in code

--- HelloWorldSolution/HelloWorld/Program.cs
+++ HelloWorldSolution/HelloWorld/Program.cs
@@ -1,8 +1,5 @@
 Console.WriteLine("Hello World!");
 Console.Write("Hello World!");
 Console.Write("Welcome to the world");
-<<<<<<< HEAD
 Console.Write("This is my code");
-=======
 Console.Write("Hey Developer");
->>>>>>> origin/main
