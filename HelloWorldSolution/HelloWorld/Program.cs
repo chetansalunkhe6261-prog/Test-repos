a0694e72a3327216ef089e0d2ae9cd144a3c4663
chetansalunkhe6261-prog <chetan.salunkhe6261@gmail.com>
2026-09-04 13:53:22 +0530

    Update Program.cs

--- HelloWorldSolution/HelloWorld/Program.cs
+++ HelloWorldSolution/HelloWorld/Program.cs
@@ -1,8 +1,3 @@
 Console.WriteLine("Hello World!");
 Console.Write("Hello World!");
 Console.Write("Welcome to the world");
-Console.WriteLine("Hello Developer");
-<<<<<<< HEAD
-
-=======
->>>>>>> 30406324a2939d499e4b2e9eb01c7001da1384db
