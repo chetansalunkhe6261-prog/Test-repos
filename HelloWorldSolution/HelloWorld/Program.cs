780be993223755c3b26df2ef269eb65178b78288
chetansalunkhe6261-prog <chetan.salunkhe6261@gmail.com>
2026-09-04 16:10:05 +0530

    vskfgh

--- HelloWorldSolution/HelloWorld/Program.cs
+++ HelloWorldSolution/HelloWorld/Program.cs
@@ -1,8 +1,8 @@
 Console.WriteLine("Hello World!");
 Console.Write("Hello World!");
 Console.Write("Welcome to the world");
-<<<<<<< HEAD
-Console.Write("This is my code");
-=======
-Console.Write("Hey Developer");
->>>>>>> origin/main
+Console.Write("Welcome to the world");
+Console.Write("Welcome to the world");
+Console.Write("Welcome to the world");
+
+Console.Write("nfjdskbjsbadr");
